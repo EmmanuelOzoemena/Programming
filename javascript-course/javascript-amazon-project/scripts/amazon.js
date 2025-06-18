2e8af6d0f5e1258e231c97d7082db1ba9e71bfbf
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-06-18 10:57:30 +0100

    Added "Added" message display on product cart addition

--- javascript-course/javascript-amazon-project/scripts/amazon.js
+++ javascript-course/javascript-amazon-project/scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png" />
             Added
           </div>
@@ -61,9 +61,9 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
+    const { productId } = button.dataset;
 
-    let matchingItem
+    let matchingItem;
 
     cart.forEach((item) => {
       if (productId === item.productId) {
@@ -71,15 +71,17 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       }
     });
 
-    const quantitySelector =  document.querySelector(`.js-quantity-selector-${productId}`)
-    const quantity = Number(quantitySelector.value)
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+    const quantity = Number(quantitySelector.value);
 
     if (matchingItem) {
       matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity: 1,
+        productId,
+        quantity,
       });
     }
 
@@ -88,6 +90,15 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     cart.forEach((item) => {
       cartQuantity += item.quantity;
     });
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add("added-to-cart-visible");
+
+    setTimeout(() => {
+      addedMessage.classList.remove("added-to-cart-visible");
+    }, 1500);
 
     document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
