aaa5744da547b8cc5a8f6d44dcfa81979ece9b64
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-06-20 17:22:34 +0100

    Convert the cart quantity from string to number

--- javascript-course/javascript-amazon-project/data/cart.js
+++ javascript-course/javascript-amazon-project/data/cart.js
@@ -62,7 +62,7 @@ export function addToCart(productId) {
   const quantitySelector = document.querySelector(
     `.js-quantity-selector-${productId}`
   );
-  const quantity = Number(quantitySelector);
+  const quantity = Number(quantitySelector.value);
 
   if (matchingItem) {
     matchingItem.quantity += quantity;
