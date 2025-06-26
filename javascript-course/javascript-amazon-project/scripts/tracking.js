075dbc2208d7775c7cff1a060114a5e2ad7dfbf8
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-06-26 16:06:22 +0100

    Made the progress bar interactive

--- javascript-course/javascript-amazon-project/scripts/tracking.js
+++ javascript-course/javascript-amazon-project/scripts/tracking.js
@@ -1,19 +1,17 @@
-import {getOrder} from '../data/orders.js';
-import {getProduct, loadProductsFetch} from '../data/products.js';
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { getOrder } from "../data/orders.js";
+import { getProduct, loadProductsFetch } from "../data/products.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 async function loadPage() {
   await loadProductsFetch();
 
   const url = new URL(window.location.href);
-  const orderId = url.searchParams.get('orderId');
-  const productId = url.searchParams.get('productId');
+  const orderId = url.searchParams.get("orderId");
+  const productId = url.searchParams.get("productId");
 
   const order = getOrder(orderId);
   const product = getProduct(productId);
 
-  // Get additional details about the product like
-  // the estimated delivery time.
   let productDetails;
   order.products.forEach((details) => {
     if (details.productId === product.id) {
@@ -21,14 +19,20 @@ async function loadPage() {
     }
   });
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress =
+    ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
     </a>
     <div class="delivery-date">
-      Arriving on ${
-        dayjs(productDetails.estimatedDeliveryTime).format('dddd, MMMM D')
-      }
+      Arriving on ${dayjs(productDetails.estimatedDeliveryTime).format(
+        "dddd, MMMM D"
+      )}
     </div>
     <div class="product-info">
       ${product.name}
@@ -38,22 +42,28 @@ async function loadPage() {
     </div>
     <img class="product-image" src="${product.image}">
     <div class="progress-labels-container">
-      <div class="progress-label">
+       <div class="progress-label ${
+         percentProgress < 50 ? "current-status" : ""
+       }">
         Preparing
       </div>
-      <div class="progress-label current-status">
+       <div class="progress-label ${
+         percentProgress >= 50 && percentProgress < 100 ? "current-status" : ""
+       }">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${
+        percentProgress >= 100 ? "current-status" : ""
+      }">
         Delivered
       </div>
     </div>
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+        <div class="progress-bar" style="width: ${percentProgress}%;"></div>
     </div>
   `;
 
-  document.querySelector('.js-order-tracking').innerHTML = trackingHTML;
+  document.querySelector(".js-order-tracking").innerHTML = trackingHTML;
 }
 
 loadPage();
